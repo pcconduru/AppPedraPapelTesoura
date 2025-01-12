b45986709a147266404a5ef3bf80229a30ac8b92
Paulo César Conduru <133069658+pcconduru@users.noreply.github.com>
2025-01-12 02:28:55 -0300

    Atualização Aplicativo Finalizado
    Feito todos os scripts e verificações para o funcionamento do aplicativo, e finalizado em sua versão definitiva

--- app/src/main/java/com/example/apppedrapapeltesoura/MainActivity.java
+++ app/src/main/java/com/example/apppedrapapeltesoura/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.apppedrapapeltesoura;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +26,59 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void pedra(View view){
+        verificarGanhador("Pedra");
+    }
+    public void papel(View view){
+        verificarGanhador("Papel");
+    }
+    public void tesoura(View view){
+        verificarGanhador("Tesoura");
+    }
+    //Escolha do BOT - Aleatória
+    public String bot(){
+        String[] opcoes = {
+                "Pedra","Papel","Tesoura"
+        };
+        int numeroAleatorio = new Random().nextInt(3);
+
+        ImageView botImage = findViewById(R.id.bot_image);
+        String escolhaBot = opcoes[numeroAleatorio];
+
+        switch (escolhaBot){
+            case "Pedra":
+                botImage.setImageResource(R.drawable.pedra);
+                break;
+            case "Papel":
+                botImage.setImageResource(R.drawable.papel);
+                break;
+            case "Tesoura":
+                botImage.setImageResource(R.drawable.tesoura);
+                break;
+        }
+        return escolhaBot;
+    }
+    //Escolha do Usuario como parametro para os metedos de cima
+    public void verificarGanhador(String usuario){
+        String escolhaBot = bot();
+        TextView textResultado = findViewById(R.id.text_resultado);
+        if(
+                (escolhaBot == "Pedra" && usuario == "Tesoura") ||
+                (escolhaBot == "Papel" && usuario == "Pedra") ||
+                (escolhaBot == "Tesoura" && usuario == "Papel")
+        ){
+            textResultado.setText("Você Perdeu! :(");
+        }
+        else if(
+                (usuario == "Pedra" && escolhaBot == "Tesoura") ||
+                (usuario == "Papel" && escolhaBot == "Pedra") ||
+                (usuario == "Tesoura" && escolhaBot == "Papel")
+        ){
+            textResultado.setText("Você Ganhou! :)");
+        }
+        else{
+            textResultado.setText("Empatou! :/");
+        }
     }
 }
